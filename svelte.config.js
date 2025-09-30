e03a65b82aa1682b95e1aacb06d2aaaeae70087b
Reece <reeceharris@email.com>
2025-09-30 14:17:08 +0100

    fix: add prerender configuration to handle HTTP errors with warnings

--- svelte.config.js
+++ svelte.config.js
@@ -5,6 +5,9 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 const config = {
   preprocess: vitePreprocess(),
   kit: {
+    prerender: {
+      handleHttpError: 'warn'
+    },
     adapter: adapter({
       fallback: undefined,
       precompress: false,
